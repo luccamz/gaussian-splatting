a2449ee0cd3cbb67ec86e651f8f210ce2abafdb2
Lucca Moura Zoppi Maia <l50060511@china.huawei.com>
2026-06-22 10:18:23 +0200

    fix: split build.ps1 into prep_env.ps1 and actual build

--- build.ps1
+++ build.ps1
@@ -1,21 +1,4 @@
-$env:CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8"
-$env:CUDA_HOME = $env:CUDA_PATH
-$env:PATH = "$env:CUDA_PATH\bin;$env:PATH"
-
-function Invoke-VcVars {
-    $tempFile = [System.IO.Path]::GetTempFileName()
-    cmd /c "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat`" x64 -vcvars_ver=14.28 && set > `"$tempFile`""
-    Get-Content $tempFile | ForEach-Object {
-        if ($_ -match '^(.*?)=(.*)$') {
-            Set-Item -Path "env:$($Matches[1])" -Value $Matches[2]
-        }
-    }
-    Remove-Item $tempFile
-}
-Invoke-VcVars
-
-$env:CUDAHOSTCXX = (Get-Command cl.exe).Source
-$env:DISTUTILS_USE_SDK = 1
+./prep_env.ps1
 
 python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
